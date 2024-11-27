fbe246e9dd64af5b00af75bf6d4692919dbf3c71
yaswants <yaswants722@gmail.com>
2024-11-28 00:23:40 +0530

    minor UI catalogue changes

--- lib/pages/catalogue.dart
+++ lib/pages/catalogue.dart
@@ -16,25 +16,46 @@ class MyHomePage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    // Watch the product provider
     final productList = ref.watch(productProvider);
+
+    // Fetch the products after the widget is initialized
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (productList.isEmpty) {
+        ref.read(productProvider.notifier).fetchProducts();
+      }
+    });
 
     double width = MediaQuery.sizeOf(context).width;
     double height = MediaQuery.sizeOf(context).height;
+    final cart = ref.watch(cartProvider);
 
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Center(child: Text(title)),
         actions: [
-          IconButton(
-            icon: const Icon(Icons.shopping_cart),
-            tooltip: 'Open shopping cart',
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const CartPage()),
-              );
-            },
+          // Badge with the number of distinct items in the cart
+          badges.Badge(
+            badgeContent: Text(
+              '${cart.length}', // Number of distinct items in the cart
+              style: const TextStyle(color: Colors.white, fontSize: 12),
+            ),
+            position: badges.BadgePosition.topEnd(top: 5, end: 5),
+            showBadge: cart.isNotEmpty, // Show badge only if the cart is not empty
+            badgeStyle: const badges.BadgeStyle(
+              badgeColor: Colors.red, // Badge color
+            ),
+            child: IconButton(
+              icon: const Icon(Icons.shopping_cart),
+              tooltip: 'Open shopping cart',
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const CartPage()),
+                );
+              },
+            ),
           ),
         ],
       ),
@@ -51,11 +72,11 @@ class MyHomePage extends ConsumerWidget {
               itemCount: productList.length,
               itemBuilder: (context, index) {
                 final product = productList[index];
+                // Watch the cart provider to see if the product is in the cart
+                final cartItem = ref.watch(cartProvider)[product.id];
 
-                // Calculate discounted price
                 double discountedPrice = product.price *
-                    (1 - product.discountPercentage / 100);
-
+                    (1 - product.discountPercentage / 100);  
                 return Card(
                   elevation: 5,
                   shape: RoundedRectangleBorder(
@@ -78,7 +99,6 @@ class MyHomePage extends ConsumerWidget {
                         padding: const EdgeInsets.all(8.0),
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
                             Text(
                               product.title,
@@ -90,7 +110,6 @@ class MyHomePage extends ConsumerWidget {
                               overflow: TextOverflow.ellipsis,
                             ),
                             Text('Brand: ${product.brand}'),
-                            // Display the original price with strikethrough
                             Row(
                               children: [
                                 Text(
@@ -99,7 +118,6 @@ class MyHomePage extends ConsumerWidget {
                                     decoration: TextDecoration.lineThrough,
                                   ),
                                 ),
-                                // Display the discounted price
                                 SizedBox(width: width * 0.01,),
                                 Text(
                                   '\$${discountedPrice.toStringAsFixed(2)}',
@@ -113,22 +131,49 @@ class MyHomePage extends ConsumerWidget {
                           ],
                         ),
                       ),
-                      ElevatedButton(
-                        onPressed: () {
-                          ref.read(cartProvider.notifier).addToCart(product);
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.purple.shade100,
-                          elevation: 1,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          padding: EdgeInsets.symmetric(
-                              horizontal: 20, vertical: 12),
-                          minimumSize: Size(150, 25),
-                        ),
-                        child: const Text('Add to Cart +'),
-                      ),
+                      cartItem == null
+                          ? ElevatedButton(
+                              onPressed: () {
+                                // Add the product to the cart
+                                ref.read(cartProvider.notifier).addToCart(product);
+                              },
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.purple.shade100,
+                                elevation: 1,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                padding: const EdgeInsets.symmetric(
+                                  horizontal: 20,
+                                  vertical: 12,
+                                ),
+                                minimumSize: const Size(150, 25),
+                              ),
+                              child: const Text('Add to Cart +'),
+                            )
+                          : Row(
+                              mainAxisSize: MainAxisSize.min,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                IconButton(
+                                  icon: const Icon(Icons.remove),
+                                  onPressed: () {
+                                    ref
+                                        .read(cartProvider.notifier)
+                                        .reduceQuantity(product.id);
+                                  },
+                                ),
+                                Text(cartItem.quantity.toString()),
+                                IconButton(
+                                  icon: const Icon(Icons.add),
+                                  onPressed: () {
+                                    ref
+                                        .read(cartProvider.notifier)
+                                        .addToCart(product);
+                                  },
+                                ),
+                              ],
+                            ),
                     ],
                   ),
                 );
